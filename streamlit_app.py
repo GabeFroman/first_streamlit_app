d76250312719ddb4aa624278fe65276c3c7f10c4
GabeFroman <115800631+GabeFroman@users.noreply.github.com>
2022-10-15 13:42:31 -0500

    Update streamlit_app.py
    removed header, fixed text

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #Allow the end user to ad a fruit to the list
-add_my_fruit = streamlit.header("What fruit would you like to add")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
