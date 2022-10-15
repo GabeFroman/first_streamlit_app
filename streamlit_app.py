5d2528c99c9dc3d68fcaccb8beeb4d75a1885bdf
GabeFroman <115800631+GabeFroman@users.noreply.github.com>
2022-10-15 13:41:20 -0500

    Update streamlit_app.py
    2nd attempt at 2nd choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #Allow the end user to ad a fruit to the list
-add_my_fruit = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+add_my_fruit = streamlit.header("What fruit would you like to add")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
